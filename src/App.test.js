360d4ecee5afe2e507aa6b9bbf0ec2f277cc51db
james.joe <kyeun950830@gmail.com>
2023-06-26 12:15:34 +0900

    initial

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter start at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
+
+test("minus botton has correct text", () => {
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리멘트에 접근 (접근할 때 ID로 접근)
+  const buttonElement = screen.getByTestId("minus-button");
+  // id가 counter인 엘리멘트가 -인지 테스트
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus botton has correct text", () => {
+  render(<App />);
+  // screen object를 이용해서 원하는 엘리멘트에 접근 (접근할 때 ID로 접근)
+  const buttonElement = screen.getByTestId("plus-button");
+   // id가 counter인 엘리멘트가 -인지 테스트
+  expect(buttonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("When the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test('on/off button has blue color', () =>{
+  render(<App/>);
+  const buttonElement = screen.getByTestId("on/off-button");
+  expect(buttonElement).toHaveStyle({backgroundColor: "blue"});
+})
+
+test("Prevent the -,+ button from being pressed when the on/off button is clicked", ()=>{
+  render(<App/>);
+  const onOffButtonElement = screen.getByTestId("on/off-button");
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toBeDisabled();
+})
